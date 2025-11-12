7505211c28385661825461fdb9d52d17c99ef0fe
jugal99 <jugalshaktawat99@gmail.com>
2025-11-13 00:11:06 +0530

    one

--- Test_Xcode/ViewController.swift
+++ Test_Xcode/ViewController.swift
@@ -11,7 +11,7 @@ class ViewController: UIViewController {
     var someValue:Int?
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Hello Sir ji ju ")
+        print("Hello Sir ji jugal ")
         // Do any additional setup after loading the view.
     }
 
